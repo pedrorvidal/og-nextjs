87078878fbb28c7da73b77bb8d3ede6f4a9f8678
Pedro Vidal <pedrorvidal@gmail.com>
2025-01-30 22:35:27 -0300

    basic routes and components

---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Página SOBRE',
+  description: 'Esta é a página SOBRE',
+};
+export default function SobrePage() {
+  return (
+    <>
+      <h2>Sobre</h2>
+    </>
+  );
+}
