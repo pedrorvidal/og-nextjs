0fcdab6bea8dfaf1df14386f000ac7e85c57ae2d
Pedro Vidal <pedrorvidal@gmail.com>
2025-02-18 19:53:05 -0300

    server and client fetch differences
    made 2 components, using server fetch and client fetch each one.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Acessos from '@/components/acessos'; //server component
 import Width from '@/components/width'; // client component
+import ServerFetch from './components/serverFetch';
 
 export default async function HomePage() {
   return (
     <main>
       <h1>Home</h1>
-      <Width />
-      <Acessos />
+      <ServerFetch />
+      {/* <Width /> */}
+      {/* <Acessos /> */}
     </main>
   );
 }
--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
+import ClientFetch from '@/components/clientFetch';
 import Width from '@/components/width';
 export default function SobrePage() {
   return (
     <main>
       <h2>Sobre:</h2>
+      <ClientFetch />
       <Width />
+
     </main>
   );
 }
