b5e359c9a3d6d6579d3b9d4e29cb63052a89aed7
Pedro Vidal <pedrorvidal@gmail.com>
2025-02-21 00:14:39 -0300

    added id to product page

--- src/app/produtos/[id]/page.tsx
+++ src/app/produtos/[id]/page.tsx
@@ -22,6 +22,7 @@ export default async function ProdutoPage({ params }: PageParams) {
       <h1>{data.nome}</h1>
       <h2>R$ {data.preco}</h2>
       <p>{data.descricao}</p>
+      <p>ID: {data.id}</p>
     </div>
   );
 }
