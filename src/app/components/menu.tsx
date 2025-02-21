3cb4ae520800445b34cdfa3b91c6497d9c77ef37
Pedro Vidal <pedrorvidal@gmail.com>
2025-02-20 23:50:38 -0300

    Intro dynamic routes

--- src/app/components/menu.tsx
+++ src/app/components/menu.tsx
@@ -15,7 +15,9 @@ export default function Menu() {
       <li>
         <Link href="/imc">IMC</Link>
       </li>
-
+      <li>
+        <Link href="/produtos">Produtos</Link>
+      </li>
     </ul>
   );
 }
