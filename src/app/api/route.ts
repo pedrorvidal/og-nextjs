92631e2008fb2ca2b6727354b76ad0fcdf86cc9c
Pedro Vidal <pedrorvidal@gmail.com>
2025-03-12 01:22:32 -0300

    rest api

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,21 +1,20 @@
-import { cookies } from "next/headers";
+import { cookies } from 'next/headers';
+import { NextRequest } from 'next/server';
 
 export async function GET() {
-  const response = await fetch('https://api.origamid.online/conta/login',
-    {
-      method: 'POST',
+  const response = await fetch('https://api.origamid.online/conta/login', {
+    method: 'POST',
 
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        username: "dog",
-        password: "dog",
-      }),
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: 'dog',
+      password: 'dog',
+    }),
 
-      cache: 'no-cache'
-    }
-  );
+    cache: 'no-cache',
+  });
 
   const data = await response.json();
 
@@ -29,4 +28,11 @@ export async function GET() {
   });
 
   return Response.json(data);
+}
+
+export async function POST(request: NextRequest) {
+  const param = request.nextUrl.searchParams.get('busca');
+  const body = await request.json();
+
+  return Response.json({ method: 'POST', response: param, body });
 }
