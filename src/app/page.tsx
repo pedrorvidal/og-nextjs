794c35b44c33218fd8cedd0bcde7f0dbfb98b9e6
Pedro Vidal <pedrorvidal@gmail.com>
2025-04-07 22:42:34 -0300

    server actions mostrada na home da aplicação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-export default async function HomePage() {
+'use client';
+
+import { setCookie } from '../../actions/set-cookies';
+
+export default function HomePage() {
+  function handleClick() {
+    console.log('Button clicked');
+    setCookie('segredo', '123456');
+  }
   return (
     <main>
       <h1>Home</h1>
+      <button onClick={handleClick}>Definir cookie</button>
     </main>
   );
 }
